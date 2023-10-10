86d8ac13c54b687eefbaab87fc2e34ba6946155b
Abe Omaits <aomaits@gmail.com>
2023-10-10 12:10:22 -0500

    created svelte components and added to layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,28 @@
 <script>
   import "../app.css";
+  import Footer from "../components/Footer.svelte";
+  import Header from "../components/Header.svelte";
+
+    let y
+    let innerHeight = 0
+    let innerWidth = 0
+
+    function goTop() {
+        document.body.scrollIntoView()
+    }
 </script>
 
-<slot />
+<div class="relative flex flex-col max-w-[1400px] mx-auto w-full text-sm sm:text-base min-h-screen">
+    <!-- if y > 0 user has scrolled down, setting the ternary for what happens if yes followed by if no-->
+    <div class={"fixed bottom-0 w-full duration-200 flex p-10 z-[10] " + (y > 0 ? ' opacity-full pointer-events-auto': ' pointer-events-none opacity-0 ')}>
+        <button>
+            <!-- Solid arrow that we'll use to scroll to the top of the page -->
+            <i class="fa-solid fa-arrow-up">
+        </button>
+    </div>
+    <Header/>
+    <slot />
+    <Footer/>
+</div>
+
+<svelte:window bind:scrollY={y} bind:innerHeight bind:innerWidth />
