563fd030510349191098567da911afb2b9cfec7d
Abe Omaits <aomaits@gmail.com>
2023-10-11 14:02:28 -0500

    Added about me section

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -13,7 +13,7 @@
     <h1 class="font-medium">
         <b class="font-bold poppins">Abe</b> Omaits
     </h1>
-    <div class="sm:flex items-center gap-4 hidden">
+    <div class="sm:flex ml-auto pr-4 items-center gap-4 hidden">
         {#each tabs as tab, index}
             <a href={tab.link} class="duration-200 hover:text-yellow-400">
                 <p>{tab.name}</p>
