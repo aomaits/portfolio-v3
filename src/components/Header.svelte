7621f6b616ad57e6f70c2368d251fd04e5953959
Abe Omaits <aomaits@gmail.com>
2023-10-11 11:17:18 -0500

    adding to the main page

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -21,7 +21,7 @@
         {/each}
     </div>
     <a href="https://www.linkedin.com/in/abe-omaits/" target="_blank" class="blueShadow relative overflow-hidden px-5 py-2 group rounded-full bg-white text-blue-950">
-        <div class="absolute top-0 right-full w-full h-full bg-neutral-600 opacity-20 group-hover:translate-x-full z-0 duration-200">
+        <div class="absolute top-0 right-full w-full h-full bg-yellow-400 opacity-20 group-hover:translate-x-full z-0 duration-200">
         </div>
         <h4 class="relative z-9">Get in touch</h4>
     </a>
--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -1,2 +1,23 @@
-Abe
+<script>
 
+</script>
+
+<main class="flex flex-col flex-1 p-4">
+    <section id="introPage" class="grid grid-cols-1 lg:grid-cols-2 gap-10 py-8 sm:py-14">
+        <div class="flex flex-col lg:justify-center text-center lg: text-left gap-6 md:gap-8 lg:gap-10">
+            <h2 class="font-semibold text-4xl sm:text-5xl md:text-6xl">
+                Hi there! <br/> I'm  <span class="poppins text-yellow-400">Abe Omaits</span>,  <br/> Full Stack Developer!
+            </h2>
+            <p class="text-base sm:text-lg md:text-xl">
+                Some of my <span class="text-yellow-400">favorite tech</span> includes JavaScript, CSS / TailwindCSS, Node.js + Express.js & SQL / MongoDB.
+            </p>
+            <a class="blueShadow mx-auto lg:mr-auto text-base sm:text-lg md:text-xl poppins relative overflow-hidden px-6 py-3 group rounded-full bg-white text-blue-950">
+                <div class="absolute top-0 right-full w-full h-full bg-yellow-400 opacity-20 group-hover:translate-x-full z-0 duration-200">
+        </div>
+        <h4 class="relative z-9">Get in touch&rarr;</h4>
+            </a>
+        </div>
+
+    </section>
+
+</main>
