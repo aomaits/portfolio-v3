6f47ef57328b2991b0608b653b1f05105ac1fbfd
Abe Omaits <aomaits@gmail.com>
2023-10-11 11:28:43 -0500

    added profile photo to portfolio

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -11,13 +11,15 @@
             <p class="text-base sm:text-lg md:text-xl">
                 Some of my <span class="text-yellow-400">favorite tech</span> includes JavaScript, CSS / TailwindCSS, Node.js + Express.js & SQL / MongoDB.
             </p>
-            <a class="blueShadow mx-auto lg:mr-auto text-base sm:text-lg md:text-xl poppins relative overflow-hidden px-6 py-3 group rounded-full bg-white text-blue-950">
+            <a class="blueShadow mx-auto lg:mr-auto text-base sm:text-lg md:text-xl poppins relative overflow-hidden px-6 py-3 group rounded-full bg-white text-blue-950 cursor-pointer" href="https://www.linkedin.com/in/abe-omaits/" target="_blank">
                 <div class="absolute top-0 right-full w-full h-full bg-yellow-400 opacity-20 group-hover:translate-x-full z-0 duration-200">
         </div>
         <h4 class="relative z-9">Get in touch&rarr;</h4>
             </a>
         </div>
-
+        <div class="relative shadow-2xt grid place-items-center">
+            <img src={"images/Abe.jpg"} alt="Abe Omaits profile" class="object-cover z-[2] max-h-[70vh]"/>
+        </div>
     </section>
 
 </main>
