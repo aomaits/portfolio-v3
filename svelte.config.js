01d7685e1a0a084a1e7d66097d1d65c5bba06ddc
Abe Omaits <aomaits@gmail.com>
2023-10-12 12:23:54 -0500

    attempting to fix svelte and github not communicating

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,12 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter()
+    adapter: adapter({
+      target: '#svelte',
+      strict: false,
+    })
   },
   preprocess: vitePreprocess()
 };
