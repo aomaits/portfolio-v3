99e86e7a42b6668b60b18eb59d1a31ac52f8ce09
Abe Omaits <aomaits@gmail.com>
2023-10-13 10:43:16 -0500

    another try at deployment, this with vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,15 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter()
+    adapter: adapter({
+      runtime: 'nodejs18.x',
+    })
   },
   preprocess: vitePreprocess()
 };
 
-config.paths = { base: process.argv.includes('portfolio-v3') ? '' : process.env.BASE_PATH }
+config.paths = { base: process.argv.includes('/portfolio-v3/') ? '' : process.env.BASE_PATH }
 
 export default config;
